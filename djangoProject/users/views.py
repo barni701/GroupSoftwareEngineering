bbc3671e05851aa22d41ddf239fa66cfbe414b56
barna <aranyi.barna@gmail.com>
2025-02-10 15:32:22 +0000

    user login and signup

--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after successful signup
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'users/templates/registration/signup.html', {'form': form})
