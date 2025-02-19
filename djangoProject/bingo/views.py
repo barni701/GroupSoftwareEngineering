f31dd42b0c09635a5781e5b9de035915f6a7e2bb
adamb00001 <ajbrooks.email@gmail.com>
2025-02-19 21:23:14 +0000

    cell links

--- djangoProject/bingo/views.py
+++ djangoProject/bingo/views.py
@@ -5,23 +5,14 @@ import random
 def make_board(rows,cols):
     # static board
     board = [
-        [{"status": 0, "challenge": "Challenge 1"}, {"status": 0, "challenge": "Challenge 2"}, {"status": 1, "challenge": "Challenge 3"}, {"status": 1, "challenge": "Challenge 4"}],
-        [{"status": 0, "challenge": "Challenge 5"}, {"status": 1, "challenge": "Challenge 6"}, {"status": 1, "challenge": "Challenge 7"}, {"status": 1, "challenge": "Challenge 8"}],
-        [{"status": 0, "challenge": "Challenge 9"}, {"status": 0, "challenge": "Challenge 10"}, {"status": 1, "challenge": "Challenge 11"}, {"status": 1, "challenge": "Challenge 12"}],
-        [{"status": 0, "challenge": "Challenge 13"}, {"status": 1, "challenge": "Challenge 14"}, {"status": 1, "challenge": "Challenge 15"}, {"status": 1, "challenge": "Challenge 16"}],
+        [{"status": 0, "challenge": "Challenge 1", "url": "https://google.com"}, {"status": 0, "challenge": "Challenge 2", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 3", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 4", "url": "https://google.com"}],
+        [{"status": 0, "challenge": "Challenge 5", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 6", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 7", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 8", "url": "https://google.com"}],
+        [{"status": 0, "challenge": "Challenge 9", "url": "https://google.com"}, {"status": 0, "challenge": "Challenge 10", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 11", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 12", "url": "https://google.com"}],
+        [{"status": 0, "challenge": "Challenge 13", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 14", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 15", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 16", "url": "https://google.com"}],
     ]
     return board
     #return array
 
 def bingo_view(request):
     board = make_board(rows=5, cols=5)
-    '''
-    board = [
-    [1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-    ]
-    '''
     return render(request, 'bingo.html', {'board': board})
