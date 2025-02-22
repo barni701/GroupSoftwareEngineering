1a086310cf8d1951964c11d1a45756adee559992
adamb00001 <93986203+adamb00001@users.noreply.github.com>
2025-02-22 13:44:06 +0000

    comments

--- djangoProject/bingo/views.py
+++ djangoProject/bingo/views.py
@@ -3,7 +3,9 @@ import random
 # Create your views here.
 
 def make_board(rows,cols):
-    # static board
+    # status: complete or incomplete
+    # challenge: the text displayed
+    # url: links to the challenge page
     board = [
         [{"status": 0, "challenge": "Challenge 1", "url": "https://google.com"}, {"status": 0, "challenge": "Challenge 2", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 3", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 4", "url": "https://google.com"}],
         [{"status": 0, "challenge": "Challenge 5", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 6", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 7", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 8", "url": "https://google.com"}],
@@ -11,7 +13,6 @@ def make_board(rows,cols):
         [{"status": 0, "challenge": "Challenge 13", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 14", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 15", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 16", "url": "https://google.com"}],
     ]
     return board
-    #return array
 
 def bingo_view(request):
     board = make_board(rows=5, cols=5)
