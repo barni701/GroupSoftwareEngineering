e25fb8eca64aadaf3d77117bb57328013ff518f9
barna <aranyi.barna@gmail.com>
2025-02-22 13:47:17 +0000

    updated the signup and views

--- djangoProject/users/views.py
+++ djangoProject/users/views.py
@@ -13,6 +13,27 @@ def signup(request):
             form.save()
             return redirect('login')  # Redirect to login after successful signup
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
+    return render(request, 'registration/signup_new.html', {'form': form})
 
-    return render(request, 'users/templates/registration/signup.html', {'form': form})
+
+def landing(request):
+    return render(request, 'users/landing.html')
+
+
+def privacy_policy(request):
+    return render(request, 'users/tc.html')
+
+logger = logging.getLogger(__name__)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        new_email = request.POST.get('email')
+
+        logger.info(f"User {user.username} changed email to {new_email}")  # Logs changes
+
+        user.email = new_email
+        user.save()
+
+    return render(request, 'profile.html')
