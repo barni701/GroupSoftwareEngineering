524545351cf78ee352ff3111f5371ad4f4e32999
adamb00001 <93986203+adamb00001@users.noreply.github.com>
2025-02-22 13:54:47 +0000

    link to game1

--- djangoProject/bingo/views.py
+++ djangoProject/bingo/views.py
@@ -7,7 +7,7 @@ def make_board(rows,cols):
     # challenge: the text displayed
     # url: links to the challenge page
     board = [
-        [{"status": 0, "challenge": "Challenge 1", "url": "https://google.com"}, {"status": 0, "challenge": "Challenge 2", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 3", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 4", "url": "https://google.com"}],
+        [{"status": 0, "challenge": "Challenge 1", "url": "/game1.html"}, {"status": 0, "challenge": "Challenge 2", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 3", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 4", "url": "https://google.com"}],
         [{"status": 0, "challenge": "Challenge 5", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 6", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 7", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 8", "url": "https://google.com"}],
         [{"status": 0, "challenge": "Challenge 9", "url": "https://google.com"}, {"status": 0, "challenge": "Challenge 10", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 11", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 12", "url": "https://google.com"}],
         [{"status": 0, "challenge": "Challenge 13", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 14", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 15", "url": "https://google.com"}, {"status": 1, "challenge": "Challenge 16", "url": "https://google.com"}],
