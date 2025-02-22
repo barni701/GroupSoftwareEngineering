cffa353bce6fc36a8e20778c23be15485482b50e
adamb00001 <93986203+adamb00001@users.noreply.github.com>
2025-02-22 14:08:20 +0000

    tests

--- djangoProject/bingo/tests.py
+++ djangoProject/bingo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class BingoViewTests(TestCase):
+
+    # Checks if the bingo view returns the status code 200
+    def test_bingo_status_page(self):
+        response = self.client.get(reverse('bingo'))
+        self.assertEqual(response.status_code, 200)
+
+    # Checks the bingo.html template is used
+    def test_bingo_template(self):
+        response = self.client.get(reverse('bingo'))
+        self.assertTemplateUsed(response, 'bingo.html')
+
+    # Checks the bingo page contains the table, which is the board
+    def test_bingo_content(self):
+        response = self.client.get(reverse('bingo'))
+        self.assertContains(response, '<table class="board" border="1">')
