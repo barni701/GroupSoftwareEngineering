4804278093c66b0e525bc56a81f6b51e4e18c1b5
Samuel <160143980+LookKilla@users.noreply.github.com>
2025-02-24 16:08:49 +0000

    Added 2fa library

--- djangoProject/SoftwareDevProject/settings.py
+++ djangoProject/SoftwareDevProject/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "users",
     "home",
     "game1",
+
+    # 2fa Auth apps
+    'django_otp',
+    'django_otp.plugins.otp_totp',  # TOTP for Google Authenticator
+    'two_factor',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +55,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    # Middleware for 2FA
+    'django_otp.middleware.OTPMiddleware',
 ]
 
 ROOT_URLCONF = "SoftwareDevProject.urls"
